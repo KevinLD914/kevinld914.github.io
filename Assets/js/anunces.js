eed03679ecd28b597cd71068a91b688ca00b0309
KevinLD914 <134461010+KevinLD914@users.noreply.github.com>
2023-05-24 19:44:31 -0300

    Add files via upload

---
+++ Assets/js/anunces.js
@@ -0,0 +1,11 @@
+function Ads(params) {
+    atOptions = {
+        'key' : '4d12606cfbdf7ad3d7a44b15a37e5980',
+        'format' : 'iframe',
+        'height' : 60,
+        'width' : 468,
+        'params' : {}
+    };
+    document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/4d12606cfbdf7ad3d7a44b15a37e5980/invoke.js"></scr' + 'ipt>');
+    
+}
